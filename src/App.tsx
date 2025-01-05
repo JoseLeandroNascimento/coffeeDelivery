7dde667b5363fa36ac61c8098d2c014fa39ae401
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-01-05 16:46:17 -0500

    style: theme e style global

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { ThemeProvider } from "styled-components"
 import { HomePage } from "./pages/home"
+import { defaultTheme } from "./styles/themes/default"
 
 
 function App() {
 
   return (
-
-    <HomePage />
+    <ThemeProvider theme={defaultTheme}>
+      <HomePage />
+    </ThemeProvider>
 
   )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,12 +1,14 @@
 
-const theme = {
-    
-    "yellow-100":"#F1E9C9",
-    "yellow-300":"#DBAC2C",
-    "yellow-900":"#C47F17",
+import { createGlobalStyle } from "styled-components";
 
-    "purple-100":"#EBE5F9",
-    "purple-300":"#8047F8",
-    "purple-900":"#4B2995",
+export const GlobalStyle = createGlobalStyle`
 
-}
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "Roboto" ,sans-serif;
+    }
+
+
+`
