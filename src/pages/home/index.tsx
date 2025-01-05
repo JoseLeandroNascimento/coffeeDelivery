dd022170bbbee8b4600f17680a68995745f75596
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-01-05 16:27:04 -0500

    feat(header): criando span localização

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,23 @@
-import { Header } from "../../components/header";
+import logo from "../../assets/logo.svg"
+import { MapPin } from "@phosphor-icons/react"
+import { Header, LocaleIndicator } from "./style"
 
-export function HomePage(){
+export function HomePage() {
 
     return (
         <>
-            <Header/>
+            <Header>
+                <img src={logo} alt="logo do coffee delivery" />
+                <div>
+                    <LocaleIndicator>
+                        <MapPin size={24} weight="fill" />
+                        Porto Alegre, RS
+                    </LocaleIndicator>
+                </div>
+            </Header>
+
+            <p></p>
+
         </>
     )
 }
---
+++ src/pages/home/style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components"
+
+export const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    padding: 2rem 10rem;
+`
+
+export const LocaleIndicator = styled.span`
+    padding: .8rem 1rem;
+    background: #EBE5F9 ;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: .25rem;
+    color: #8047F8;
+    border-radius: 8px;
+    cursor: pointer;
+`
+
+
+export const CardButton = styled.button`
+
+
+
+`
