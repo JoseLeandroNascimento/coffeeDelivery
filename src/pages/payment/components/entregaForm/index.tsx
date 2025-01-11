d4aa360bf2e6c8c1c232cf56fdfe131be2e5c331
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-01-11 11:49:41 -0500

    feat(form entrega): add cabeçalho no formulário

--- src/pages/payment/components/entregaForm/index.tsx
+++ src/pages/payment/components/entregaForm/index.tsx
@@ -1,35 +1,51 @@
+import { MapPinLine } from "@phosphor-icons/react";
 import { InputText } from "../../../../styles/global";
-import { EntregaFormContainer, Group1, Group2, Group3, Group4 } from "./style";
+import { CardFormEntrega, EntregaFormContainer, Group1, Group2, Group3, Group4, HeaderForm, SubtitleFormEntrega, TitleFormEntrega } from "./style";
 
 export function EntregaForm() {
     return (
+        <CardFormEntrega>
 
-        <form >
-            <EntregaFormContainer>
+            <HeaderForm>
+                <MapPinLine size={20} />
+                <div>
+                    <TitleFormEntrega>Endereço de Entrega</TitleFormEntrega>
+                    <SubtitleFormEntrega>Informe o endereço onde deseja receber seu pedido</SubtitleFormEntrega>
+                </div>
+            </HeaderForm>
 
-                <Group1>
-                    <InputText placeholder="CEP" />
-                </Group1>
+            <form >
 
-                <Group2>
-                    <InputText placeholder="Rua" />
-                </Group2>
-
-                <Group3>
-                    <InputText placeholder="Número" />
-                    <InputText placeholder="Complemento" />
-                </Group3>
-
-                <Group4>
-                    <InputText placeholder="Bairro" />
-
-                    <InputText placeholder="Cidade" />
-
-                    <InputText placeholder="UF" />
-                </Group4>
+                <EntregaFormContainer>
 
 
-            </EntregaFormContainer>
-        </form>
+                    <Group1>
+                        <InputText placeholder="CEP" />
+                    </Group1>
+
+                    <Group2>
+                        <InputText placeholder="Rua" />
+                    </Group2>
+
+                    <Group3>
+                        <InputText placeholder="Número" />
+                        <InputText placeholder="Complemento" />
+                    </Group3>
+
+                    <Group4>
+                        <InputText placeholder="Bairro" />
+
+                        <InputText placeholder="Cidade" />
+
+                        <InputText placeholder="UF" />
+                    </Group4>
+
+
+                </EntregaFormContainer>
+            </form>
+
+        </CardFormEntrega>
+
+
     )
 }
--- src/pages/payment/components/entregaForm/style.ts
+++ src/pages/payment/components/entregaForm/style.ts
@@ -1,17 +1,45 @@
 import styled from "styled-components";
 
-
-export const EntregaFormContainer = styled.div`
-
+export const CardFormEntrega = styled.div`
     width: 40rem;
-    background-color: ${props=>props.theme["gray-200"]};
+    background-color: ${props => props.theme["gray-200"]};
     padding: 2.5rem;
     border-radius: 6px;
+
+`
+
+export const EntregaFormContainer = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1rem;
     
 `
+
+export const HeaderForm = styled.div`
+
+    display: grid;
+    grid-template-columns: auto 1fr;
+    gap: .715rem ;
+    margin-bottom: 2rem;
+
+    & svg{
+
+        color: ${props => props.theme["yellow-900"]};;
+    }
+
+`
+
+export const TitleFormEntrega = styled.h3`
+    font-size: 1rem;
+    font-weight: 500;
+    color: ${props => props.theme["gray-600"]};
+`
+
+export const SubtitleFormEntrega = styled.p`
+    font-size: .875rem;
+    color: ${props => props.theme["gray-400"]};
+`
+
 
 export const Group1 = styled.div`
 
